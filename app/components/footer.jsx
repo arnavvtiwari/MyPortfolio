15dfd25894a2f1bbcc4b8b2b8c745c620b12611b
Arnav Tiwari <arnav.luck@gmail.com>
2024-10-26 15:19:19 +0530

    changes

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Arnav Tiwari</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/arnavtiwari/" className="text-[#16f2b3]">Arnav Tiwari</Link>
           </p>
           <div className="flex items-center gap-5">
             
